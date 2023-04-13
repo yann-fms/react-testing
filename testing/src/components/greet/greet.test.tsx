8a7bb25d377f4de44cf4610ac8b96c5cefea236e
yann-fms <yann.deor@fms-ea.com>
2023-04-13 12:13:43 +0200

    feat: add great component and test

--- testing/src/components/greet/greet.test.tsx
+++ testing/src/components/greet/greet.test.tsx
@@ -1,8 +1,20 @@
-import { render,screen } from "@testing-library/react"
-import Greet from './greet';
+/**
+ * Great chould render the text "Hello" and if a name is passed into the component
+ * It should rendre "hello" followed by the ame assed to the component
+ */
 
-test('Geet renders correctly', () =>{
+import { render, screen } from "@testing-library/react"
+import Greet from "./greet"
+
+test('Greet render correctly', ()=>{
     render(<Greet />)
-    const textElement = screen.getByText('Hello');
+   const textElement = screen.getByText('Hello')
+   expect(textElement).toBeInTheDocument();
+
+})
+
+test('Greet render with a name', () => {
+    render(<Greet name='John'/>)
+    const textElement = screen.getByText('Hello John')
     expect(textElement).toBeInTheDocument();
 })
