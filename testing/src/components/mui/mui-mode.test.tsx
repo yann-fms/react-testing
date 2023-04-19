382180beba743e41a55bf533f1b7620c8287fc60
yann-fms <yann.deor@fms-ea.com>
2023-04-19 11:01:02 +0200

    feat(test): add a provider to all test (usefull for redux provider)

--- testing/src/components/mui/mui-mode.test.tsx
+++ testing/src/components/mui/mui-mode.test.tsx
@@ -1,12 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../test-utils';
 import { MuiMode } from './MuiMode'
 import { AppProviders } from '../../providers/AppProviders';
 
 describe('Mui', () => {
     test('render test correctly', () => {
-        render(<MuiMode/>, {
-            wrapper: AppProviders
-        })
+        render(<MuiMode/>)
         const headingElement = screen.getByRole('heading')
         expect(headingElement).toHaveTextContent("dark mode")
     })
